259e960797db8c76b899f726afa56a87674d51a3
klpaulse <lovise_paulsen@hotmail.com>
2026-06-03 21:22:06 +0200

    router installert

---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return( 
+        <>
+        <h1>Faktaopplysninger:</h1>
+        <ul>
+            <li>Henrik suger i å spille Reinhardt på overwatch</li>
+            <li>Karianne er sykt god i overwatch og flink til å bygge på minecraft</li>
+        </ul>
+        </>
+
+    )
+}
